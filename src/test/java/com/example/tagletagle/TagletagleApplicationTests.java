cf79f2a0a11e40c92f8dce91d2c1c198e40c0d72
eclipse021 <yahang2008@naver.com>
2025-02-06 06:26:34 +0900

    fix: 배포 버전으로 세팅

--- src/test/java/com/example/tagletagle/TagletagleApplicationTests.java
+++ src/test/java/com/example/tagletagle/TagletagleApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.tagletagle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/*@SpringBootTest
 class TagletagleApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
-}
+}*/
